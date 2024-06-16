87bac86f02f6d52b759d6109f459994d230dcdd0
Arlyjr <arly.lcj@gmail.com>
2024-06-16 18:07:04 -0300

    attach_detach

--- Dado_attach_detach.R
+++ Dado_attach_detach.R
@@ -4,3 +4,14 @@ Dado <- data.frame(x = 1, mean = "Jr")
 attach(dado)
 detach(dado)
 
+#Quando necesssitamos fazer muitas referências para um objeto, podemos…
+#… minimizar a digitação, colocando o data.frame ou lista no caminho de procura, através do comando
+
+#> attach(dados)
+#> search()
+#Isso permite referenciar os componentes de uma lista , por exemplo, simplesmente digitando
+
+#> nome
+#Cuidado!
+  
+  #Se você possui objetos com o mesmo nome em diferentes listas, isso pode causar alguma confusão. Antes de começar outra análise com outros dados, não esqueça de usar a função detach().
